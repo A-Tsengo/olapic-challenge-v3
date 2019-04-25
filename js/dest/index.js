057a5d0395706463a395eef2d73469c7bfbc2947
James Chen <atlas.tsengo@gmail.com>
2019-04-24 20:52:26 -0700

    js refactor.  encapsulated all code in IFFE

--- js/dest/index.js
+++ js/dest/index.js
@@ -1,3 +1,52 @@
 "use strict";
 
-console.log("foo");
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+(function () {
+    var olapicApi = "https://photorankapi-a.akamaihd.net/customers/215757/media/recent?rights_given=1&include_tagged_galleries=0&auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2";
+    var mediaArray = [];
+    var vm = new Vue({
+        el: "#app",
+        data: {
+            responses: "",
+            groups: [],
+            seen: true
+        }
+    });
+    var moreImages = document.querySelector('#more');
+    var showImages = function showImages() {
+        if (vm.responses.length > 0) {
+            vm.groups.push(vm.responses[0]);
+            vm.responses = vm.responses.slice(1);
+            if (vm.responses.length == 0) {
+                vm.seen = false;
+            }
+        }
+    };
+    moreImages.addEventListener("click", function (e) {
+        e.preventDefault();
+        showImages();
+    });
+    axios.get(olapicApi).then(function (data) {
+        // console.log(data.data.data._embedded.media)
+        var mediaArray = [].concat(_toConsumableArray(data.data.data._embedded.media)).map(function (element) {
+            return element.images.normal;
+        });
+        var mediaArrayGrouped = [];
+
+        function buildGroup(array) {
+            var subArray = [];
+            for (var i = 0; i < 5; i++) {
+                subArray.push(array[i]);
+            }
+            array = array.slice(5);
+            mediaArrayGrouped.push(subArray);
+            if (array.length > 0) {
+                buildGroup(array);
+            };
+        };
+        buildGroup(mediaArray);
+        vm.responses = mediaArrayGrouped;
+        showImages();
+    });
+})();
--- js/src/index.js
+++ js/src/index.js
@@ -1,58 +1,47 @@
-// recent
+(function(){
 const olapicApi = "https://photorankapi-a.akamaihd.net/customers/215757/media/recent?rights_given=1&include_tagged_galleries=0&auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2";
-
-// photorank
-// const olapicApi = "https://photorankapi-a.akamaihd.net/customers/215757/media/photorank?rights_given=1&include_tagged_galleries=0&auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2";
-
-// rated 
-// const olapicApi = "https://photorankapi-a.akamaihd.net/customers/215757/media/rated?rights_given=1&include_tagged_galleries=0&auth_token=0a40a13fd9d531110b4d6515ef0d6c529acdb59e81194132356a1b8903790c18&version=v2.2";
 let mediaArray = [];
-
-var vm = new Vue({
+const vm = new Vue({
     el: "#app",
     data: {
         responses: "",
         groups: [],
-        seen: true 
+        seen: true
     }
-  })
-
+})
+const moreImages = document.querySelector('#more');
+const showImages = () => {
+    if (vm.responses.length > 0) {
+        vm.groups.push(vm.responses[0]);
+        vm.responses = vm.responses.slice(1);
+        if (vm.responses.length == 0) {
+            vm.seen = false;
+        }
+    }
+}
+moreImages.addEventListener("click", (e) => {
+    e.preventDefault();
+    showImages();
+})
 axios.get(olapicApi)
-.then(data => { 
-    console.log(data.data.data._embedded.media)
-   const mediaArray = [...data.data.data._embedded.media].map(element => element.images.normal);
-   let mediaArrayGrouped = [];
-   function buildGroup(array){
-       const subArray = [];
-       for(let i=0; i<5; i++){
-           subArray.push(array[i]);
-       }
-       array = array.slice(5);
-       mediaArrayGrouped.push(subArray);
-       if(array.length > 0){
-           buildGroup(array);
-       };
-   };
-   buildGroup(mediaArray);
-   vm.responses = mediaArrayGrouped;
+    .then(data => {
+        // console.log(data.data.data._embedded.media)
+        const mediaArray = [...data.data.data._embedded.media].map(element => element.images.normal);
+        let mediaArrayGrouped = [];
 
-   vm.groups.push(vm.responses[0]); 
-   vm.responses = vm.responses.slice(1);
-
-   });
-
-   const moreImages = document.querySelector('#more');
-
-   moreImages.addEventListener("click", (e) => {
-       e.preventDefault();
-       
-  if(vm.responses.length > 0){ 
-   vm.groups.push(vm.responses[0]); 
-   vm.responses = vm.responses.slice(1);
-   if(vm.responses.length == 0){
-       vm.seen = false;
-    // document.querySelector('#more').classList.add('hidden')
-   }
-  }
-   })
-
+        function buildGroup(array) {
+            const subArray = [];
+            for (let i = 0; i < 5; i++) {
+                subArray.push(array[i]);
+            }
+            array = array.slice(5);
+            mediaArrayGrouped.push(subArray);
+            if (array.length > 0) {
+                buildGroup(array);
+            };
+        };
+        buildGroup(mediaArray);
+        vm.responses = mediaArrayGrouped;
+        showImages();
+    });
+})();
